143177160f1ec48147d912532ed36a5f4b9628ce
SatyabrataPaital <satyabrata02@gmail.com>
2018-01-27 01:35:53 +0530

    project commited

--- student/src/main/java/student/FirstMaven.java
+++ student/src/main/java/student/FirstMaven.java
@@ -2,9 +2,43 @@ package student;
 
 public class FirstMaven {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private int sid;
+	private String sname;
+	private String semail;
+	private int roll;
+	
+	
+	
+	public FirstMaven() {
+		
 	}
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public String getSemail() {
+		return semail;
+	}
+	public void setSemail(String semail) {
+		this.semail = semail;
+	}
+	public int getRoll() {
+		return roll;
+	}
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+	
+	
+	
+
 
 }
